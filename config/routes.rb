852f40c6bda06714f20eeb759113eba915400c45
seiyaf <seiyafukusima@gmail.com>
2022-09-11 13:40:05 +0900

    [Add] index

---
+++ config/routes.rb
@@ -0,0 +1,29 @@
+Rails.application.routes.draw do
+  get 'people/index'
+  get 'msgboard/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  
+  get 'hello/index'
+  get 'hello', to: 'hello#index'
+  get 'hello/other'
+  post 'hello', to: 'hello#index'
+  post 'hello/index'
+
+  get 'msgboard', to: 'msgboard#index'
+  post 'msgboard', to: 'msgboard#index'
+  get 'msgboard/index'
+  post 'msgboard/index'
+
+  get 'people', to: 'people#index'
+  get 'people/add'
+  post 'people/add', to: 'people#create'
+  get 'people/edit/:id', to: 'people#edit'
+  post 'people/edit/:id', to: 'people#update'
+  patch 'people/edit/:id', to: 'people#update'
+  get 'people/delete/:id', to: 'people#delete'
+  get 'people/:id', to: 'people#show'
+  
+end
